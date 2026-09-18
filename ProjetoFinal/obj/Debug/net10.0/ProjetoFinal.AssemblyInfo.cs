ecf4733437a81d80198e6b761a16b93c600a3925
Théo Vinícius Boaretto Peia <theo.boaretto@gmail.com>
2026-09-17 21:23:14 -0300

    Finalização do Layout Cadastro e Melhoria em Layout Login

--- ProjetoFinal/obj/Debug/net10.0/ProjetoFinal.AssemblyInfo.cs
+++ ProjetoFinal/obj/Debug/net10.0/ProjetoFinal.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865f7ddac45e1c08f9d573a09a2b6fad1d064d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
